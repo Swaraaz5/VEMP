9fc7fcefb6d814cebf3f7076090eb25c71c04e2d
Swaraj <swarajpurekar007@gmail.com>
2022-12-17 15:09:57 +0530

    User Profile Card 85%

--- client/src/components/User/UserProfileSettings.jsx
+++ client/src/components/User/UserProfileSettings.jsx
@@ -6,12 +6,19 @@ function UserProfileSettings() {
   };
   return (
     <>
-      <div className="m-auto md:mt-5 md:px-40 ">
+      <div className="m-auto  md:px-40 ">
         <form onSubmit={onFormSubmit}>
           <div className="block p-5 rounded-lg shadow-lg bg-red-500 max-w-xl m-auto content-center text-center">
 
+            <div className="bg-blue-300 mx-56 h-20 flex justify-center items-center text-center mb-3">
+                Hello
+            </div>   
 
-              <div className="flex form-group mb-6 items-center justify-center bg-yellow-300">
+            <div className="flex justify-center items-center text-center mb-3">
+               <p className="text-2xl"> Swaraj Purekar </p>
+            </div>
+
+              <div className="flex form-group mb-3 items-center justify-center bg-yellow-300">
                 <label>Date of Birth :</label> 
                 <input
                   type="text"
@@ -36,7 +43,7 @@ function UserProfileSettings() {
                 />
               </div>
 
-              <div className="flex form-group mb-6 items-center justify-center bg-yellow-300">
+              <div className="flex form-group mb-3 items-center justify-center bg-yellow-300">
                 <label>Gender :</label> 
                 <input
                   type="text"
@@ -62,15 +69,17 @@ function UserProfileSettings() {
                 />
               </div>
 
+              <div className="flex form-group mb-3 items-center justify-center bg-yellow-300">
+                <label>Address :</label> 
 
-
-{/* 
-              <div className="form-group mb-6">
-                <textarea
+               <textarea
                   className="
                     form-control
                     block
-                    w-full
+                    ml-10
+                    md:w-2/3
+                    w-2/3
+                    mx-2
                     px-3
                     py-1.5
                     text-base
@@ -89,30 +98,107 @@ function UserProfileSettings() {
                   placeholder="Message"
                   defaultValue={""}
                 />
-              </div> */}
+              </div>
+
+              <div className="flex form-group mb-3 items-center justify-center bg-yellow-300">
+                <label>Mobile No :</label> 
+                <input
+                  type="text"
+                  className="form-control block
+                    md:w-2/3
+                    ml-5
+                    w-2/3
+                    mx-2
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleInput7"
+                  placeholder="Name"
+                />
+              </div>
+
+              <div className="flex form-group mb-3 items-center justify-center bg-yellow-300">
+                <label>Department :</label> 
+                <input
+                  type="text"
+                  className="form-control block
+                    md:w-2/3
+                    w-2/3
+                    mx-2
+                    ml-3
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleInput7"
+                  placeholder="Name"
+                />
+              </div>
+
+              <div className="flex form-group mb-3 items-center justify-center bg-yellow-300">
+                <label>Email :</label> 
+                <input
+                  type="text"
+                  className="form-control block
+                    md:w-2/3
+                    w-2/3
+                    mx-2
+                    ml-14
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleInput7"
+                  placeholder="Name"
+                />
+              </div>
       
               <button
                 type="submit"
                 className="
-     
-      px-6
-      py-2.5
-      bg-blue-600
-      text-white
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      rounded
-      shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out"
+                px-6
+                py-2.5
+                bg-blue-600
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                rounded
+                shadow-md
+                hover:bg-blue-700 hover:shadow-lg
+                focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                active:bg-blue-800 active:shadow-lg
+                transition
+                duration-150
+                ease-in-out"
               >
-                Send
+                Update Profile
               </button>
 
 
