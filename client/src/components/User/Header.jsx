db9a5d820883c885923448d02d575ed6e1bc6433
Swaraj <swarajpurekar007@gmail.com>
2022-12-17 12:17:15 +0530

    Added User Profile Icons and Dropdown

--- client/src/components/User/Header.jsx
+++ client/src/components/User/Header.jsx
@@ -1,19 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Cookie from 'js-cookie'
-import ls from 'localstorage-slim';
+import Cookie from "js-cookie";
+import ls from "localstorage-slim";
 
-function Header({toggleNav}) {
+function Header({ toggleNav }) {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    Cookie.remove('EmpData');
+    Cookie.remove("EmpData");
     navigate("/emplogin");
-    ls.remove('empID');
+    ls.remove("empID");
   };
+
+  const [profileHS, setProfileHS] = useState(false)
+  const toggleProfileHS = () => {
+    console.log("sadasda")
+    setProfileHS(current => !current)
+  }
   return (
     <Fragment>
-      <nav class="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 flex">
+      <nav class="relative px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 flex">
         <button
           onClick={toggleNav}
           data-collapse-toggle="navbar-dropdown"
@@ -37,7 +43,69 @@ function Header({toggleNav}) {
             ></path>
           </svg>
         </button>
-        <div class="container flex flex-wrap items-center justify-end mx-auto">
+
+        <div class="container flex items-center justify-end mx-auto">
+
+          {/* User Profile Dropdown */}
+        <div class="flex items-center">
+          <button
+            onClick={()=>toggleProfileHS()}
+            type="button"
+            className="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            id="user-menu-button"
+            aria-expanded="false"
+            data-dropdown-toggle="user-dropdown"
+            data-dropdown-placement="bottom"
+          >
+            <span class="sr-only">Open user menu</span>
+            <img class="w-12 h-8 rounded-full" src="/img/profile-default.png" alt="user photo" />
+          </button>
+
+          <div
+            class={"absolute top-14 right-16 z-50 block my-4 text-base list-none divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 bg-yellow-300 " + (profileHS ? " block" : "hidden")}
+            id="user-dropdown"
+          >
+            <div class="px-4 py-3">
+              <span class="block text-sm text-gray-900 dark:text-white">
+                Swaraj Purekar
+              </span>
+              <span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+                swaraj@gmail.com
+              </span>
+            </div>
+            <ul class="py-1" aria-labelledby="user-menu-button">
+
+              <li>
+                <a
+                  href="#"
+                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Profile Settings
+                </a>
+              </li>
+               {/* <li>
+                <a
+                  href="#"
+                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Dashboard
+                </a>
+              </li> */}
+              {/* <li>
+                <a
+                  href="#"
+                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >
+                  Sign out
+                </a>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+
+
+
+        {/* Logout Button */}
           <div class=" w-full md:block md:w-auto" id="navbar-dropdown">
             <ul class="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
               <li>
